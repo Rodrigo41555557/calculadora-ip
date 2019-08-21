fbf0edd1c911b53633269ce1eef7e3d4fd59e2bd
Future Man 623 <37418306+Rodrigo41555557@users.noreply.github.com>
2019-08-21 14:29:09 -0300

    ajustes nos cálculos e nomes

--- funcip.php
+++ funcip.php
@@ -2,122 +2,122 @@
 
 //recebe dados do formulario e faz os calculos
 
-$grupo1  = (int)$_POST['grupo1'];
-$grupo2  = (int)$_POST['grupo2'];
-$grupo3  = (int)$_POST['grupo3'];
-$grupo4  = (int)$_POST['grupo4'];
-$mascara = (int)$_POST['mascara'];
 
+	$grupo1  = (int)$_POST['grupo1'];
+	$grupo2  = (int)$_POST['grupo2'];
+	$grupo3  = (int)$_POST['grupo3'];
+	$grupo4  = (int)$_POST['grupo4'];
+	$mascara = (int)$_POST['mascara'];
 
+	$bits = 32 - $mascara;
+	$ipsPorRede = pow(2, $bits);
+	$subredes = 256/$ipsPorRede;
+	$mascDecimal = 256 - $ipsPorRede;
 
-$bits = 32 - $mascara;
+	$redes = (int)($grupo4/$ipsPorRede);
+	$endRede = $redes*$ipsPorRede;
+	$endBroadcast = $endRede+$ipsPorRede - 1;
+	
+	$primIP = $redes*$ipsPorRede + 1;
+	$ultIP = $endBroadcast - 1;
 
-$ipsPorRede = pow(2, $bits);
+	function classeIP($grupo1){
+		
+		if ($grupo1 >= "0" and $grupo1 <= "126") {
+			$classe = "A";
+		}elseif ($grupo1 >= "128" and $grupo1 <= "191") {
+			$classe = "B";
+		}elseif ($grupo1 >= "192" and $grupo1 <= "223") {
+			$classe = "C";
+		}elseif ($grupo1 >= "224" and $grupo1 <= "239") {
+			$classe = "D";
+		}elseif ($grupo1 >= "240" and $grupo1 <= "255") {
+			$classe = "E";
+		}else{
+			$classe = "Localhost, Não pertence à nenhuma classe";
+		}
+		return $classe;
+	}
 
-$subredes = 256/$ipsPorRede;
-
-$redes = (int)($grupo4/$ipsPorRede);
-
-$endRede = $redes*$ipsPorRede;
-
-$primIP = $redes*$ipsPorRede + 1;
-
-$endBroadcast = $endRede+$ipsPorRede - 1;
-
-$ultIP = $endBroadcast - 1;
-
-$mascDecimal = 256 - $ipsPorRede;
-
-function classeIP($grupo1)
-{
-    if ($grupo1 >= "0" and $grupo1 <= "126") {
-        $classe = "A";
-    }elseif ($grupo1 >= "128" and $grupo1 <= "191") {
-        $classe = "B";
-    }elseif ($grupo1 >= "192" and $grupo1 <= "223") {
-        $classe = "C";
-    }elseif ($grupo1 >= "224" and $grupo1 <= "239") {
-        $classe = "D";
-    }elseif ($grupo1 >= "240" and $grupo1 <= "255") {
-        $classe = "E";
-    }else{
-        $classe = "Localhost, Não pertence à nenhuma classe";
-    }
-    return $classe;
-}
+	
+	if ($mascara = 31){
+		$primIP = $endRede;
+		$ultIP = $endBroadcast;
+	}
 ?>
 
-<nav class="navbar  navbar-light mx-auto bordao" >                      
-            
-        <form method="post" class="form-inline my-2 my-lg-0 mx-auto" action="">
+	<nav class="navbar  navbar-light mx-auto bordao" >                      
+				
+			<form method="post" class="form-inline my-2 my-lg-0 mx-auto" action="">
 
-            <ul class="navbar-nav mx-auto">
+				<ul class="navbar-nav mx-auto">
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h4>Resultados para o IP: <?= $grupo1.".".$grupo2.".".$grupo3.".".$grupo4." /".$mascara ?></h4>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h4>Resultados para o IP: <?= $grupo1.".".$grupo2.".".$grupo3.".".$grupo4." /".$mascara ?></h4>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>IPs por rede: <?= $ipsPorRede ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Hosts para rede: <?= $ipsPorRede ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Número de Sub-redes: <?= $subredes ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Número de Sub-redes: <?= $subredes ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Número de Redes: <?= $redes ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Número de Redes: <?= $redes ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Endereço de rede: <?= $endRede ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Endereço de rede: <?= $grupo1.".".$grupo2.".".$grupo3.".".$endRede ?></h8>
+						</ul>
+					</li>
+					
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Endereço Broadcast: <?= $grupo1.".".$grupo2.".".$grupo3.".".$endBroadcast ?></h8>
+						</ul>
+					</li>
+					
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Classe do IP: <?= classeIP($grupo1) ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Broadcast: <?= $endBroadcast ?></h8>
-                    </ul>
-                </li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Primeiro IP Válido: <?= $primIP ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Primeiro IP Válido: <?= $grupo1.".".$grupo2.".".$grupo3.".".$primIP ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Último IP Válido: <?= $ultIP ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Último IP Válido: <?= $grupo1.".".$grupo2.".".$grupo3.".".$ultIP ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Máscara: 255.255.255.<?= $mascDecimal ?></h8>
-                    </ul>
-                </li>
+					<li class="nav-item mx-auto">
+						<ul class="nav-link text-light">
+							<h8>Máscara: 255.255.255.<?= $mascDecimal ?></h8>
+						</ul>
+					</li>
 
-                <li class="nav-item mx-auto">
-                    <ul class="nav-link text-light">
-                        <h8>Classe do IP: <?= classeIP($grupo1) ?></h8>
-                    </ul>
-                </li>
+				</ul>
 
-            </ul>
+			</form>
 
-        </form>
-
-    </nav>
+		</nav>
 
 
 
--- index.php
+++ index.php
@@ -11,13 +11,14 @@
 <body >
 	<br>
 
-<div class="container">
+<div class="container"></div>
 
-    <h3 class="aligner"> <b>Calculadora de IP</b> </h3>
-    <h6 class="aligner">Digite o IP e sua Máscara</h6>
+    <h3 class="aligner text-light"> <b>Calculadora de IP</b> </h3>
+    <h6 class="aligner text-light">Digite o IP e sua Máscara</h6>
 
 
-    <div id="divisor"></div>
+
+<div class="divisor container"></div>
 
     <nav class="navbar navbar-expand-lg navbar-primary  mx-auto" >						
 			
@@ -25,32 +26,32 @@
 
 			<ul class="navbar-nav mx-auto">
 
-				<li class="nav-item">
+				<li class="nav-item mx-auto">
 					<ul class="nav-link text-light">
 						<label>Grupo 1</label>
 						<input type="number" name="grupo1" min="0" max="255" required="">
 					</ul>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item mx-auto">
 					<ul class="nav-link text-light">
 						<label>Grupo 2</label>
 						<input type="number" name="grupo2" min="0" max="255" required="">
 					</ul>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item mx-auto">
 					<ul class="nav-link text-light">
 						<label>Grupo 3</label>
 						<input type="number" name="grupo3" min="0" max="255" required="">
 					</ul>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item mx-auto">
 					<ul class="nav-link text-light">
 						<label>Grupo 4</label>
 						<input type="number" name="grupo4" min="0" max="255" required="">
 					</ul>
 				</li>
 
-				<li class="nav-item">
+				<li class="nav-item mx-auto">
 					<ul class="nav-link text-light">
 						<label>Máscara do IP</label>
 						<input type="number" name="mascara" min="24" max="32" required="">
@@ -58,20 +59,25 @@
 			    </li>
 
 			</ul>
-
+			
+ <div class=" container"></div>
+			
 			<ul class="navbar-nav mx-auto">
 
-			<li class="nav-item">
-			    <ul class="nav-link text-dark">
-			    	<input type="submit" name="ipenviado" value="Calcular" >
-			    </ul>
-			</li>
+				<li class="nav-item ">
+					<ul class="nav-link text-dark">
+						<input type="submit" name="ipenviado" value="Calcular" >
+					</ul>
+				</li>
 
 			</ul>
 
 		</form>
 
 	</nav>
+
+ <div class="divisor container"></div>
+
 <?php
 	require 'funcip.php';
 ?>
